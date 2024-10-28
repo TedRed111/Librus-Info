8f6f3c2f42a0859a08a60339f75fa70a4e36a2c1
TedRed111 <tedred111@yahoo.com>
2024-10-28 11:15:03 +0100

    встраиваем страницу

--- sw.js
+++ sw.js
@@ -1,65 +1,23 @@
-const CACHE_NAME = 'librus-info-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icons/android-chrome-192x192.png',
-    './icons/android-chrome-512x512.png',
-    './icons/apple-touch-icon.png',
-    './icons/favicon-16x16.png',
-    './icons/favicon-32x32.png'
-];
+const CACHE_NAME = 'librus-app-v1';
 
-self.addEventListener('install', event => {
-    console.log('Service Worker installing.');
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Service Worker caching files.');
-                return cache.addAll(urlsToCache);
-            })
-            .catch(error => {
-                console.error('Service Worker cache error:', error);
-            })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json'
+        ]);
+      })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request)
-                    .then(response => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    });
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
 });
