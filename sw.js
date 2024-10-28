9bdeda3f9d441852587f677d0c0b885fcd6a6e73
TedRed111 <tedred111@yahoo.com>
2024-10-28 11:39:11 +0100

    авто навигация

--- sw.js
+++ sw.js
@@ -1,23 +1,28 @@
-const CACHE_NAME = 'librus-app-v1';
-
+// service-worker.js
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json'
-        ]);
-      })
-  );
+    event.waitUntil(
+        caches.open('librus-pwa-v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/icons/icon-192x192.png',
+                '/manifest.json'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Разрешаем cross-origin запросы
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
